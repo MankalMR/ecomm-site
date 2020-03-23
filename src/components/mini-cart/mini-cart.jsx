963433458547f316331fa44bed8b45d0d3c0df73
Manohar Mankala <manohar_mankala@anfcorp.com>
2020-03-23 06:32:48 -0400

    added cart item component - used the component in mini-cart component - connected these via cart reducer

--- src/components/mini-cart/mini-cart.jsx
+++ src/components/mini-cart/mini-cart.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-import Button from "../button/button";
+import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
 
-import "./mini-cart.scss";
+import './mini-cart.scss';
 
-const MiniCart = () => (
-  <div className="mini-cart">
-    <div className="cart-items" />
+const renderCartItems = cartItems =>
+  cartItems.map(item => <CartItem key={item.id} item={item} />);
+
+const MiniCart = ({ cartItems }) => (
+  <div className='mini-cart'>
+    <div className='cart-items'>{renderCartItems(cartItems)}</div>
     <Button>Checkout</Button>
   </div>
 );
 
-export default MiniCart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+MiniCart.propTypes = {
+  cartItems: PropTypes.array
+};
+
+export default connect(mapStateToProps)(MiniCart);
